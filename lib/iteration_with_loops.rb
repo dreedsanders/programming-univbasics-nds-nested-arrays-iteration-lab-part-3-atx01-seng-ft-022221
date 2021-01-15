627a264628ac3474d232e658c2cdfa7b71dbaa78
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-15 18:34:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -21,7 +21,7 @@ mixed_data = [
   ["studied", 101, 233, "Ruby"]
 ]
   
-  
+join_nested_strings(mixed_data)
   
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
